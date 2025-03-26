9a76185cd44bb6998ddabf86cf4be7020fe12fff
Luc Deplanche <luc.deplanche@cs-soprasteria.com>
2025-03-26 15:11:08 +0100

    WIP

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,8 +5,13 @@ set -eu
 [[ -z "${USER}" ]] && { echo "USER is not set. Use -e"; exit 1; }
 [[ -z "${USER_ID}" ]] && { echo "USER_ID is not set. Use -e"; exit 1; }
 
-# Custom user creation 
+# Zoxide repo
+ZOXIDE_REPO=https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh
+DOTFILE_REPO=https://github.com/slacking-eveywhere/dotfiles.git
+
+# Custom user creation
 export HOME=/home/${USER}
+export WORKDIR=/workdir
 
 # Check if the user exists
 USER_EXISTS=$(id -u "${USER}" 2>/dev/null || true)
@@ -22,36 +27,32 @@ elif [[ -n "${UID_EXISTS}" ]]; then
     EXISTING_USER=$(getent passwd "${USER_ID}" | cut -d: -f1)
     usermod -l "${USER}" "${EXISTING_USER}"
     usermod -d "/home/${USER}" -m "${USER}"
+
+    # Chown uid to new user
+    find "${HOME}" \
+        -path "${HOME}"/.cache -prune \
+        -o -path "${HOME}"/.config -prune \
+        -o -path "${HOME}"/.local -prune \
+        -o -exec chown -R "${USER}":"${USER}" {} +
 else
     # Create new user if both username and UID do not exist
     useradd \
         --shell /usr/bin/zsh \
-        --home-dir /home/"${USER}" \
-        --uid "${USER_ID}" \
-        "${USER}"
-    mkdir -p "${HOME}"
-    chown "${USER}":"${USER}" /workdir
+        --home-dir ${HOME} \
+        --uid ${USER_ID} \
+        ${USER}
+    mkdir -p ${HOME}
+    chown ${USER}:${USER} ${HOME} ${WORKDIR}
+
+    # Exec next commmand with gosu
+    /usr/local/bin/gosu ${USER} bash -c "\
+        cd ${HOME} ; \
+        curl -sSfL ${ZOXIDE_REPO} | sh ; \
+        git clone ${DOTFILE_REPO} .dotfiles ; \
+        cd .dotfiles ; \
+        stow --target=${HOME} --adopt */ ;"
 fi
 
-
-# Use root as skel to populate custom user
-find /root/ \
-    -mindepth 1 \
-    -maxdepth 1 \
-    -exec cp -r {} "${HOME}" \;
-
-# Chown root to new user
-find "${HOME}" \
-    -path "${HOME}"/.cache -prune \
-    -o -path "${HOME}"/.config -prune \
-    -o -path "${HOME}"/.local -prune \
-    -o -exec chown -R "${USER}":"${USER}" {} +
-
-# Remove root folder content
-rm -rf /root/*
-rm -rf /root/.*
-
-# Exec next commmand with gosu
 exec /usr/local/bin/gosu ${USER} "$@"
 
 exec "$@"
